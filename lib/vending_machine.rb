ffe5cf3051fe974d8e1ac296f32af4a25eeafe3c
SMITHG50 <smithg50@nationwide.com>
2017-07-10 14:45:03 -0400

    passing  test for display after item is selected

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,6 +1,6 @@
 class VendingMachine
 
-  attr_accessor :credits, :coin_return, :product_bin
+  attr_accessor :credits, :coin_return, :product_bin, :item_selected
 
   def initialize
     @credits = 0
@@ -8,11 +8,16 @@ class VendingMachine
   end
 
   def get_display
-    if @credits == 0
+    if item_selected
+      @item_selected = false
+      return 'THANK YOU'
+    end
+
+    if credits == 0
       return 'INSERT COINS'
     end
 
-    '$%.2f' % (@credits.to_i/100.0)
+    '$%.2f' % (credits.to_i/100.0)
   end
 
   def get_credits
@@ -25,6 +30,7 @@ class VendingMachine
 
   def select_item(item)
     @product_bin = item
+    @item_selected = true
   end
 
   def check_product_bin
@@ -32,7 +38,7 @@ class VendingMachine
   end
 
   def return_coins
-    @coin_return += @credits
+    @coin_return += credits
     @credits = 0
   end
 
