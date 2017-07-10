d4e823ac3ff50447acc8836f10472b13a7cc8056
SMITHG50 <smithg50@nationwide.com>
2017-07-09 21:00:03 -0400

    initial spec setup and first test

---
+++ spec/vending_machine_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'VendingMachine' do
+
+  describe '.get_display' do
+    context 'when no coins have been inserted' do
+      it 'returns INSERT COIN' do
+        @vm = VendingMachine.new
+
+
+
+      end
+    end
+  end
+end
