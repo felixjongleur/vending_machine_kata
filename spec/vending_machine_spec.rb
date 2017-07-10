81394e14c6d4f86af16cf38dd675251a29338a8c
SMITHG50 <smithg50@nationwide.com>
2017-07-09 21:51:01 -0400

    refactor of test suite to use a before method for object instantiation

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -2,10 +2,13 @@ require 'spec_helper'
 
 describe 'VendingMachine' do
 
+  before(:each) do
+    @vm = VendingMachine.new
+  end
+
   describe '.get_display' do
     context 'when no coins have been inserted' do
       it 'returns INSERT COINS' do
-        @vm = VendingMachine.new
         expect(@vm.get_display).to eql 'INSERT COINS'
       end
     end
@@ -14,17 +17,14 @@ describe 'VendingMachine' do
   describe '.valid_coin' do
     context 'when it is a valid coin' do
       it 'returns TRUE' do
-        @vm = VendingMachine.new
         expect(@vm.valid_coin?(Coin::NICKEL)).to be true
       end
     end
 
     context 'when it is an invalid coin' do
       it 'returns FALSE' do
-        @vm = VendingMachine.new
         expect(@vm.valid_coin?(Coin::PENNY)).to be false
       end
     end
   end
-
 end
